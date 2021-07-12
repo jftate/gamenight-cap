8d789330878e29c612daa54b8d5ce235c631f177
jftate <jean-francois.tatetsiledze@itelligence.de>
2021-07-12 12:03:17 +0000

    consume a lib on BTP

--- app/Northwind/webapp/controller/View1.controller.js
+++ app/Northwind/webapp/controller/View1.controller.js
@@ -1,33 +1,39 @@
 // @ts-nocheck
+jQuery.sap.registerModulePath("de.nttdata.utillib", "../jtelibraries.denttdatautillib-1.0.0/");
 sap.ui.define([
-    "sap/ui/core/mvc/Controller"
+    "sap/ui/core/mvc/Controller",
+    "de/nttdata/utillib/uiservice/js/utils"
 ],
 	/**
      * @param {typeof sap.ui.core.mvc.Controller} Controller
      */
-    function (Controller) {
+    function (Controller, utils) {
         "use strict";
-
+        
         return Controller.extend("de.tit.Northwind.controller.View1", {
             onInit: function () {
-
+                
                 //console.log(this.getOwnerComponent().getModel("countries").getData());
-
+                
                 /*var oCountryModel = new sap.ui.model.json.JSONModel();
                 oCountryModel.loadData("/RestCountries/rest/v2/all");
-
+                
                 oCountryModel.attachRequestCompleted(function () {
                     debugger;
                     console.log(oCountryModel.getData());
                 });*/
 
+                //jQuery.sap.registerResourcePath("de.nttdata.utillib", "uiservice/jtelibraries.denttdatautillib-1.0.0/");
+
+                //jQuery.sap.require("de.nttdata.utillib.uiservice.js.utils");
+                
                 var prefix = this.getOwnerComponent()._oManifest._oBaseUri._parts.path;
 
                 $.ajax({
                     url: prefix + "RestCountries/rest/v2/all",
                     type: "GET",
                     success: function (data, textStatus, jqXHR) {
-                        console.log(data);
+                        console.log(utils.copyArray(data));
                     },
                     error: function (jqXHR, textStatus, errorThrown) {
                         console.log('Error occurred!');
